21bb98af56a956df35cdf76b53f88c28e045fdfa
AimotoRyosuke <r.aimoto97vg@gmail.com>
2026-01-02 18:05:13 +0900

    format

--- lib/core/constants/app_theme.dart
+++ lib/core/constants/app_theme.dart
@@ -4,7 +4,10 @@ const Color _primaryColor = Color(0xFF1D3557);
 const Color _accentColor = Color(0xFFEA7C5D);
 
 final ThemeData appTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: _primaryColor, secondary: _accentColor),
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: _primaryColor,
+    secondary: _accentColor,
+  ),
   useMaterial3: true,
   scaffoldBackgroundColor: const Color(0xFFF0F5F9),
   appBarTheme: const AppBarTheme(
